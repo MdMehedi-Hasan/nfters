4a34acb0d8301a285940355262e715b5f3e016bd
MdMehedi-Hasan <mh19115@gmail.com>
2024-08-10 00:44:25 +0600

    initial config

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import PrimaryButton from "./Components/PrimaryButton";
+
+export default function App() {
+  return (
+    <div className="m-10">
+      <PrimaryButton message="Upload" />
+    </div>
+  )
+}
---
+++ src/Components/PrimaryButton.jsx
@@ -0,0 +1,5 @@
+export default function PrimaryButton({message}) {
+  return (
+      <button className='bg-blue-800 text-white px-4 py-1 rounded-full'>{message}</button>
+  )
+}
